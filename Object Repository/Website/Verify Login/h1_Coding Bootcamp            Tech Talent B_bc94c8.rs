5ddbfa3344e65557fa8edeb6d535851080cb5c7d
Dimas Arya <dimasarya96@gmail.com>
2023-02-01 16:54:11 +0700

    Commit 1 Test Case Login

---
+++ Object Repository/Website/Verify Login/h1_Coding Bootcamp            Tech Talent B_bc94c8.rs
@@ -0,0 +1,102 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<WebElementEntity>
+   <description></description>
+   <name>h1_Coding Bootcamp            Tech Talent B_bc94c8</name>
+   <tag></tag>
+   <elementGuidId>f653fdd3-5e48-4568-a438-cba2ae63f664</elementGuidId>
+   <selectorCollection>
+      <entry>
+         <key>CSS</key>
+         <value>h1.new_headline_h3</value>
+      </entry>
+      <entry>
+         <key>XPATH</key>
+         <value>//div[@id='main-body-homepage']/div/h1</value>
+      </entry>
+   </selectorCollection>
+   <selectorMethod>XPATH</selectorMethod>
+   <useRalativeImagePath>true</useRalativeImagePath>
+   <webElementProperties>
+      <isSelected>false</isSelected>
+      <matchCondition>equals</matchCondition>
+      <name>tag</name>
+      <type>Main</type>
+      <value>h1</value>
+      <webElementGuid>e02448d6-7252-4d69-bef7-6c668fcebf0c</webElementGuid>
+   </webElementProperties>
+   <webElementProperties>
+      <isSelected>false</isSelected>
+      <matchCondition>equals</matchCondition>
+      <name>class</name>
+      <type>Main</type>
+      <value>new_headline_h3</value>
+      <webElementGuid>338de2ce-125d-4125-9bb3-b2adb310f139</webElementGuid>
+   </webElementProperties>
+   <webElementProperties>
+      <isSelected>true</isSelected>
+      <matchCondition>equals</matchCondition>
+      <name>text</name>
+      <type>Main</type>
+      <value>Coding Bootcamp
+            Tech Talent Berkualitas</value>
+      <webElementGuid>1c939924-d3ec-438f-ae86-8f192ac8879f</webElementGuid>
+   </webElementProperties>
+   <webElementProperties>
+      <isSelected>false</isSelected>
+      <matchCondition>equals</matchCondition>
+      <name>xpath</name>
+      <type>Main</type>
+      <value>id(&quot;main-body-homepage&quot;)/div[@class=&quot;containerHeader&quot;]/h1[@class=&quot;new_headline_h3&quot;]</value>
+      <webElementGuid>205ffe09-d8b1-4499-bc48-b5ae98713d21</webElementGuid>
+   </webElementProperties>
+   <webElementXpaths>
+      <isSelected>true</isSelected>
+      <matchCondition>equals</matchCondition>
+      <name>xpath:idRelative</name>
+      <type>Main</type>
+      <value>//div[@id='main-body-homepage']/div/h1</value>
+      <webElementGuid>75795979-8208-42bf-bb33-0aa13fdbf616</webElementGuid>
+   </webElementXpaths>
+   <webElementXpaths>
+      <isSelected>false</isSelected>
+      <matchCondition>equals</matchCondition>
+      <name>xpath:neighbor</name>
+      <type>Main</type>
+      <value>(.//*[normalize-space(text()) and normalize-space(.)='Quality Assurance Engineer Class'])[2]/preceding::h1[1]</value>
+      <webElementGuid>c1bfc89d-94a3-4d5f-8549-eb861057e19c</webElementGuid>
+   </webElementXpaths>
+   <webElementXpaths>
+      <isSelected>false</isSelected>
+      <matchCondition>equals</matchCondition>
+      <name>xpath:neighbor</name>
+      <type>Main</type>
+      <value>(.//*[normalize-space(text()) and normalize-space(.)='Jadi QA Engineer hanya dalam 2 bulan dengan Jaminan Kerja dan Beasiswa*.'])[1]/preceding::h1[1]</value>
+      <webElementGuid>d998da68-54dc-4f3f-babf-9e8b4f50cd7b</webElementGuid>
+   </webElementXpaths>
+   <webElementXpaths>
+      <isSelected>false</isSelected>
+      <matchCondition>equals</matchCondition>
+      <name>xpath:neighbor</name>
+      <type>Main</type>
+      <value>//*/text()[normalize-space(.)='Coding Bootcamp']/parent::*</value>
+      <webElementGuid>55ca476d-cde6-4f63-af8b-3a5364594501</webElementGuid>
+   </webElementXpaths>
+   <webElementXpaths>
+      <isSelected>false</isSelected>
+      <matchCondition>equals</matchCondition>
+      <name>xpath:position</name>
+      <type>Main</type>
+      <value>//h1</value>
+      <webElementGuid>3bcdfd4c-0fa1-4412-9ee0-25ee91dea489</webElementGuid>
+   </webElementXpaths>
+   <webElementXpaths>
+      <isSelected>false</isSelected>
+      <matchCondition>equals</matchCondition>
+      <name>xpath:customAttributes</name>
+      <type>Main</type>
+      <value>//h1[(text() = 'Coding Bootcamp
+            Tech Talent Berkualitas' or . = 'Coding Bootcamp
+            Tech Talent Berkualitas')]</value>
+      <webElementGuid>7b8fdfa1-92bf-4253-9ef0-85b3649dad44</webElementGuid>
+   </webElementXpaths>
+</WebElementEntity>
